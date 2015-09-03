f4a8f03f4bd34e50941c37e11c724b1f038cb4b8
Rike-Benjamin Schuppner <rikebs@debilski.de>
2015-09-03 17:47:15 +0200

    BF: Whoops. Used old API.

--- test/test_drunk_player.py
+++ test/test_drunk_player.py
@@ -17,14 +17,15 @@ class MyPlayerTest(unittest.TestCase):
             # 2 .  . 3 #
             ############
         """
-        gm = GameMaster(test_layout, number_bots=4, game_time=5, seed=20)
+        teams = [
+            # register my_team for bots 0, 2
+            my_team,
+            # register a pre-defined team as an enemy
+            # First one moves left-down-down-left-left, second one left
+            SimpleTeam(TestPlayer("<vv<<"), TestPlayer("<<<<<"))
+        ]
 
-        # register my_team for bots 0, 2
-        gm.register_team(my_team)
-
-        # register a pre-defined team as an enemy
-        # First one moves left-down-down-left-left, second one left
-        gm.register_team(SimpleTeam(TestPlayer("<vv<<"), TestPlayer("<<<<<")))
+        gm = GameMaster(test_layout, teams, number_bots=4, game_time=5, seed=20)
 
         # play `game_time` rounds
         gm.play()
